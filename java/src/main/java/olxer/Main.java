e3e8f5c8a1c602190a39ddbdd23848269083f88c
Pavel <paulsedeen@gmail.com>
2015-11-01 12:29:06 +0200

    Working on switchable datasource

--- java/src/main/java/olxer/Main.java
+++ java/src/main/java/olxer/Main.java
@@ -3,12 +3,8 @@ package olxer;
 import java.sql.SQLException;
 import javax.xml.bind.JAXBException;
 import olxer.threading.NewAdsSearchThread;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Main {
-
-    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws SQLException, JAXBException {
 
